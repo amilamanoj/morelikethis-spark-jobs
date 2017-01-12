4f18da7a2370aefb1d21883c17345eed5bb12c23
Prateek057 <bagrecha.prateek@gmail.com>
2017-01-12 15:46:46 +0100

    New Vector Builder

---
+++ src/main/scala/de/tum/seba/morelikethis/VectorBuilder2.scala
@@ -0,0 +1,66 @@
+package de.tum.seba.morelikethis
+
+/**
+  * Created by USER on 1/12/2017.
+  */
+import java.util.Arrays
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+
+import spark.jobserver.{SparkJob, SparkJobInvalid, SparkJobValid, SparkJobValidation}
+
+import scala.util.Try
+
+object VectorBuilder2 extends SparkJob{
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[4]").setAppName("VectorBuilderJob")
+    val sc = new SparkContext(conf)
+    val config = ConfigFactory.parseString("")
+
+    val results = runJob(sc, config)
+    println("Result is " + results)
+  }
+
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
+    Try(config.getString("input.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No input.string config param"))
+  }
+
+  override def runJob(sc: SparkContext, config: Config): Any = {
+
+    val spark = SparkSession
+      .builder
+      .appName("Word2Vec example")
+      .getOrCreate()
+
+    // $example on$
+    // Input data: Each row is a bag of words from a sentence or document.
+    val documentDF = spark.createDataFrame(Seq(
+      "Hi I heard about Spark".split(" "),
+      "I wish Java could use case classes".split(" "),
+      "Logistic regression models are neat".split(" ")
+    ).map(Tuple1.apply)).toDF("text")
+
+    // Learn a mapping from words to Vectors.
+    val word2Vec = new Word2Vec()
+      .setInputCol("text")
+      .setOutputCol("result")
+      .setVectorSize(3)
+      .setMinCount(0)
+    val model = word2Vec.fit(documentDF)
+
+    val result = model.transform(documentDF)
+    result.collect().foreach { case Row(text: Seq[_], features: Vector) =>
+      println(s"Text: [${text.mkString(", ")}] => \nVector: $features\n") }
+    // $example off$
+
+    spark.stop()
+
+  }
+}
